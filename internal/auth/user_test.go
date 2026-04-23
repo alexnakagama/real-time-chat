197fe7015a0a386a9ad52cd62a6d6555797f9a14
alex <alexnakagama4@gmail.com>
2026-04-23 02:47:56 -0300

    added testing to generate and verify password

--- internal/auth/user_test.go
+++ internal/auth/user_test.go
@@ -1,1 +1,19 @@
 package auth
+
+import (
+	"testing"
+)
+
+func TestGenerateAndVerifyPasswordHash(t *testing.T) {
+	password := "mysecret"
+	hash, err := GeneratePasswordHash(password)
+	if err != nil {
+		t.Fatalf("error generating hash: %v", err)
+	}
+	if !VerifyPasswordHash(password, hash) {
+		t.Error("the password should be valid for the generated hash")
+	}
+	if VerifyPasswordHash("another", hash) {
+		t.Error("an incorrect password shouldnt be valid")
+	}
+}
